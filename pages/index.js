5daaadac7a24ea45b64dab9ceab20209f61ee0c0
kesel <a.kiselyoff@gmail.com>
2023-05-29 00:06:31 +0300

    first steps

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+const Index = () => {
+  return (
+    <div>
+      <nav className="nav">
+        <Link href="/" className="link">
+          Main
+        </Link>
+        <Link href="/users" className="link">
+          Users
+        </Link>
+      </nav>
+      <h1>Main page</h1>
+      <style jsx>{`
+        .nav {
+          background: gray;
+          padding: 15px;
+        }
+        .link {
+          text-decoration: none;
+          color: blue;
+          font-size: 20px;
+          margin: 10px;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Index;
